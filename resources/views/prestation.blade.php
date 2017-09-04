5e9eb4ca76501dab87b68f4a1316129818bea8be
Alex De Pembroke <alexandre.depembroke@campus-numerique-in-the-alps.com>
2017-09-04 11:15:32 +0200

    fix bug reponsive

--- resources/views/prestation.blade.php
+++ resources/views/prestation.blade.php
@@ -7,18 +7,14 @@
 	  			<thead>
 			      	<tr>
 				        <th>Titre</th>
-				        <th>Description</th>
 				        <th>Début</th>
-				        <th>Fin</th>
 			      </tr>
 			    </thead>
 			    <tbody>
 			@foreach($events as $event)
 			    <tr onclick="window.document.location='/agenda/{{$event->id}}';">
 			        <td>{{$event->title}}</td>
-			        <td>{{$event->content}}</td>
 			        <td>{{\Carbon\Carbon::parse($event->start_time)->format('d/m/Y')}}</td>
-			        <td>{{\Carbon\Carbon::parse($event->end)->format('d/m/Y')}}</td>
 			    </tr>
 			@endforeach
 			    </tbody>
