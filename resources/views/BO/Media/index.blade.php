dd62d38edf32a0daf11f05a0690ab6a687fcef91
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2018-05-17 16:58:06 +0200

    delete comment blade

--- resources/views/BO/Media/index.blade.php
+++ resources/views/BO/Media/index.blade.php
@@ -52,23 +52,4 @@
 
         </script>
     </div>
-    {{--@foreach($events as $event)--}}
-        {{--<div class="col-lg-4" style="height: 500px; overflow: auto; border: #0d3625 solid 1px">--}}
-            {{--<h2>{{ $event->title }}</h2>--}}
-            {{--<span class="label label-default pull-right">{{ $event->isPrivate === 0 ? 'Public' : 'Privé' }}</span>--}}
-            {{--<p class='lead'>{!! $event->content !!}</p>--}}
-            {{--<p class="lead">débute le : <span class="glyphicon glyphicon-time"></span> {{ date('d-m-Y à H:i', strtotime($event->start_time)) }}</p>--}}
-            {{--<p class="lead">finie le : <span class="glyphicon glyphicon-time"></span> {{ date('d-m-Y à H:i', strtotime($event->end_time)) }}</p>--}}
-            {{--@if (Auth::check() && Auth::user()->adminLevel > 0)--}}
-                {{--<div class="pull-right">--}}
-                    {{--<a href="/updateEvent/{{$event->id}}" class="btn btn-warning">Mettre a Jour</a>--}}
-                    {{--<form method="POST" action='/deleteEvent/{{$event->id}}'>--}}
-                        {{--<input name="_method" type="hidden" value="DELETE">--}}
-                        {{--{!! csrf_field() !!}--}}
-                        {{--<input class="btn btn-danger" type="submit" value="Supprimer">--}}
-                    {{--</form>--}}
-                {{--</div>--}}
-            {{--@endif--}}
-        {{--</div>--}}
-    {{--@endforeach--}}
 @endsection
