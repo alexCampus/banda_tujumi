a524e626295d947d5040bfe446689990fd511b5f
alexCampus <alex.depem@gmail.com>
2020-06-24 14:21:36 +0200

    ajout task scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('db:backup')->twiceDaily(13, 22);
     }
 
     /**
